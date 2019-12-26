fe65710bf778500cf47ce1b68c37787cd80e8480
Daniel <dan@coav.ru>
2019-12-26 14:40:29 +0200

    toggl report for one user

--- app/services/toggl_report.rb
+++ app/services/toggl_report.rb
@@ -8,9 +8,8 @@ class TogglReport
   end
 
   def report
-
-
-    # todo: output filtered out projects and their times
+    collect_entries
+    output_reports
   end
 
   private
@@ -19,24 +18,55 @@ class TogglReport
     @entries = {}
     rows = @api.time_entries(Time.now.beginning_of_month)
     rows.each do |row|
-      put row
-      proj_name = project(row).name
-      proj = @entries[proj_name] ||= Index.new
+      pr_name = project(row).name
+      pr = @entries[pr_name] ||= {}
 
-      entry = row.entry
       date = Time.parse(row.start).to_date
+      day = pr[date] ||= {}
+
+      entry = row.description
       duration = if row.stop and row.duration >= 0
                    row.duration
                  else
                    Time.now - started_at
                  end
+      day[entry] ||= 0
+      day[entry] += duration
     end
   end
 
-  def save_reports
-    REPORT_PROJECTS.each do ||
-
+  def output_reports
+    @entries.each do |pr_name, project|
+      total_hours = project.map do |date, day|
+        day.map do |entry, duration|
+          duration
+        end
+      end.flatten.sum
+      if pr_name.in? REPORT_PROJECTS
+        write_project_report pr_name, project
+        status = 'Write'
+      else
+        status = 'Skip'
+      end
+      puts "#{status} #{pr_name}: #{to_hours total_hours}"
     end
+  end
+
+  def write_project_report pr_name, project
+    File.open("reports/#{pr_name}.md", 'w') do |file|
+      project.each do |date, day|
+        file.write("\n### #{date.strftime "%^a %d"}\n")
+        day.sort_by do |entry, duration|
+          -duration
+        end.each do |entry, duration|
+          file.write("#{to_hours duration}: #{entry}\n")
+        end
+      end
+    end
+  end
+
+  def to_hours(seconds)
+    (seconds.to_f / 60 / 60).round(1)
   end
 
   def project(entry)
--- lib/tasks/report.rake
+++ lib/tasks/report.rake
@@ -1,6 +1,8 @@
 
 task report: :environment do
 
+  puts "Toggl report"
+  TogglReport.new.report
 
   puts :done
 end
